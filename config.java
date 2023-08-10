1e27960bf1067785e2f15042c8746dc6622862d6
sravya3710 <134071876+sravya3710@users.noreply.github.com>
2023-08-10 19:03:57 +0530

    Add files via upload

---
+++ config.java
@@ -0,0 +1,13 @@
+package com.org.genAss;
+
+import org.springframework.context.annotation.ComponentScan;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.org.genAss")
+
+
+public class config {
+
+}
