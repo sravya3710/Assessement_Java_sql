e832d75da471acf07cd12f0e3fde70da4c00b288
sravya3710 <134071876+sravya3710@users.noreply.github.com>
2023-08-10 19:02:03 +0530

    Add files via upload

---
+++ Securities.java
@@ -0,0 +1,16 @@
+package com.org.genAss;
+
+public class Securities implements Employee {
+	
+	public Securities(){
+		System.out.println("Security department issue Access details for all the employee");
+		
+	}
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
