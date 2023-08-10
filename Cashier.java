de411aa0874bcd66e6c960215af559103a4c8abc
sravya3710 <134071876+sravya3710@users.noreply.github.com>
2023-08-10 19:03:39 +0530

    Add files via upload

---
+++ Cashier.java
@@ -0,0 +1,17 @@
+package com.org.genAss;
+
+public class Cashier implements Employee {
+	
+	public Cashier(){
+		System.out.println("Casheier submitted data about cash");
+		
+	}
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
